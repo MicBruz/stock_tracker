854e3340d651dc831cdf3649639acf0c8dc2dbeb
Michal Bru <mbruzda1@wp.pl>
2021-02-16 19:17:02 +0100

    Add refresh prices functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,5 +36,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def my_portfolio_update
+    err_messages = []
+    @tracked_stocks = current_user.stocks
+    @tracked_stocks.each do |stock|
+      new_stock = Stock.new_lookup(stock.ticker)
+      if new_stock
+        if new_stock.last_price != stock.last_price
+          stock.last_price = new_stock.last_price
+        end
+        if stock.save
+          err_messages << "#{stock.ticker}: Updated"
+        else
+          err_messages << "#{stock.ticker}: DatabaseErr"
+        end
+      else
+        err_messages << "#{stock.ticker}: ConectionErr"
+      end
+    end
+    flash[:notice] = err_messages.join(", ")
+    redirect_to my_portfolio_path
+  end
 
 end
